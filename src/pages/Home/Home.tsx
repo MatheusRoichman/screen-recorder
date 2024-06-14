9eab8d1258bb173dc7b55367118b1c2529cf4cf1
EmersonSousa15 <emersonsousac14@gmail.com>
2024-06-14 18:32:14 -0300

    feat: start and stop

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,69 +1,70 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { MdScreenshotMonitor, MdOutlineVideocam, MdOutlineVideocamOff } from "react-icons/md";
 import { FaStop } from "react-icons/fa";
-import { VscDebugContinue } from "react-icons/vsc";
-import { IoMdMic, IoMdMicOff, IoIosPause } from "react-icons/io";
-
+import { IoMdMic, IoMdMicOff } from "react-icons/io";
+import { useReactMediaRecorder } from "react-media-recorder";
 
 type Controls = {
     statusRec: boolean;
-    statusPlayRecording: boolean;
-    statusMic: boolean;
+    isAudioMuted: boolean;
     statusCam: boolean;
-};
+}
 
-type ControlKeys = keyof Controls;
+type ControlKeys = keyof Controls
 
 export const Home = () => {
     const [controls, setControls] = useState<Controls>({
         statusRec: false,
-        statusPlayRecording: false,
-        statusMic: false,
+        isAudioMuted: true,
         statusCam: false
     })
 
+    const { status, startRecording, stopRecording, mediaBlobUrl } =
+        useReactMediaRecorder({ video: controls.statusCam, screen: true, audio: controls.isAudioMuted })
 
-    const handleClick = (status: ControlKeys) => {
-        setControls(prev => ({
-            ...prev, 
-            [status]: !prev[status]
-        }))
+
+    const handleClick = (action: ControlKeys) => {
+        if(status !== "recording" || action === "statusRec"){
+            setControls(prev => ({
+                ...prev,
+                [action]: !prev[action]
+            }))
+        }
+        
     }
+
+    useEffect(() => {
+        controls.statusRec ? startRecording() : stopRecording()
+
+    }, [controls])
+
+
 
 
     return (
         <main className="bg-slate-600 h-screen w-screen text-white">
+            <h1>{status}</h1>
             <section>
-                <video src=""></video>
+                <video src={mediaBlobUrl} controls autoPlay loop></video>
             </section>
             <aside>
-                <button onClick={() => handleClick('statusMic')}>
-                    {controls.statusMic ? <IoMdMic size={35} /> : <IoMdMicOff size={35} />}
+                <button onClick={() => handleClick('isAudioMuted')}>
+                    {controls.isAudioMuted && status !== "recording" ? <IoMdMicOff size={35} /> :  <IoMdMic size={35} />}
                 </button>
                 <div>
-                    {controls.statusRec ?
-                        (
-                            <>
-                                {controls.statusPlayRecording ?
-                                    <button onClick={() => handleClick('statusPlayRecording')}>
-                                        <IoIosPause size={35}/>
-                                    </button>
-                                    : <button onClick={() => handleClick('statusPlayRecording')}>
-                                        <VscDebugContinue size={35}/>
-                                    </button>
-                                }
-                                <button onClick={() => handleClick('statusRec')}>
-                                    <FaStop size={35}/>
-                                </button>
-                            </>
-                        ) :
+                    {controls.statusRec && status === "recording" ?
+
                         <button onClick={() => handleClick('statusRec')}>
-                            <MdScreenshotMonitor size={35}/>
+                            <FaStop size={35} />
+                        </button>
+                        :
+                        <button onClick={() => handleClick('statusRec')}>
+                            <MdScreenshotMonitor size={35} />
                         </button>
                     }
                 </div>
-                <button  onClick={() => handleClick('statusCam')}>
-                    {controls.statusCam ? <MdOutlineVideocam size={35}/> : <MdOutlineVideocamOff size={35}/>}
+                <button onClick={() => handleClick('statusCam')}>
+                    {controls.statusCam && status == "recording" ? <MdOutlineVideocam size={35} /> : <MdOutlineVideocamOff size={35} />}
                 </button>
             </aside>
         </main>
